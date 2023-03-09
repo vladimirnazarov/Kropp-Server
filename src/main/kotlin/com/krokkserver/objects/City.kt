e271ee81cb9c8dea977d0c01ec01eca2202dd17a
Vladimir Nazarov <tooldiamond2014@gmail.com>
2023-03-09 22:16:12 +0300

    Step 1 is finished

---
+++ src/main/kotlin/com/krokkserver/objects/City.kt
@@ -0,0 +1,24 @@
+package com.krokkserver.objects
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class City(
+    val id_locale: Int,
+
+    val id: Int,
+
+    val name: String,
+
+    val lang: Int,
+
+    val logo: String,
+
+    val last_edit_time: Long,
+
+    val visible: Boolean,
+
+    val city_is_regional: Boolean,
+
+    val region: String
+)
--- src/main/kotlin/com/krokkserver/plugins/Routing.kt
+++ src/main/kotlin/com/krokkserver/plugins/Routing.kt
@@ -6,8 +6,8 @@ import io.ktor.server.application.*
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        get("/test") {
+            call.respondText("test")
         }
     }
 }
